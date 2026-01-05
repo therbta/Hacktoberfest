455ccf1e2c81b13077386cfb389738f9b0257726
Baris Taskiran <rbtaskiran@gmail.com>
2026-01-04 20:46:54 -0500

    add throttle utility in typescript

---
+++ TypeScript/throttle.ts
@@ -0,0 +1,11 @@
+function throttle<T extends (...args: any[]) => void>(fn: T, limit: number): (...args: Parameters<T>) => void {
+  let lastCall = 0;
+  return function(...args: Parameters<T>) {
+    const now = Date.now();
+    if (now - lastCall >= limit) {
+      lastCall = now;
+      fn(...args);
+    }
+  };
+}
+export { throttle };
